4c5b83ec0c79495c0f9f0fa70a9a1f62e500dbe8
Ben Brewerton <benbrewerton98@gmail.com>
2020-05-25 06:40:14 +0100

    Added Dual Functionality

---
+++ newmain.py
@@ -0,0 +1,127 @@
+import os
+import time
+import RPi.GPIO as GPIO
+from datetime import datetime
+import spidev
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
+import board
+import digitalio
+import threading
+from newscreen import loopScreen
+
+# GPIO info
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(19, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# Define the Reset Pin
+oled_reset = digitalio.DigitalInOut(board.D18)
+
+# Open SPI bus
+spi = spidev.SpiDev()
+spi.open(0,0)
+spi.max_speed_hz = 1000000
+
+# OLED info
+WIDTH = 128
+HEIGHT = 64  # Change to 64 if needed
+BORDER = 5
+
+# Function to query the ADC
+def ReadChannel(channel):
+	# Transfer 1000 to read channel 0 with the D0-D2 bits
+    adc = spi.xfer2([1, (8+channel) << 4 , 0])
+	# Receive the bits b0-b9 from the adc sample
+    data = ((adc[1] & 3) << 8) + adc[2]
+    return data
+
+avg_count = 0 # A count of the total
+moving_avg = (2**10) / 2 # half point of a 10 bit register, the centered point
+loop_counter = 1 # Count of loop iterations
+sample_rate = 3000 # Sampling rate in Hz
+interval = 1/sample_rate # Interval between loop iterations
+
+# Threshold of stroke to background noise
+threshold = 0.3
+
+# Array for holding samples
+samples = []
+
+# Record start time of loop
+startDate = datetime.now()
+
+# Print a statement that the listeining is starting
+print("Starting DrumTime with fs: " + str(sample_rate) + "Hz and sensitivity threshold of " + str(threshold*100) + "%")
+
+def loopADC():
+	global threshold
+	global moving_avg
+	global avg_count
+	global loop_counter
+	global interval
+
+
+	while True:
+		# Read the ADC Value
+		adc_value = ReadChannel(0)
+
+		# Check to illuimate the LED if the threshold is crossed
+		if adc_value * (1-threshold) > moving_avg:
+			GPIO.output(19,GPIO.HIGH)
+			time.sleep(0.3)
+			GPIO.output(19,GPIO.LOW)
+
+		# # Check if loop count exceeds 1000000
+		#if loop_count > 100000:
+		#	loop_count = 1
+		#	avg_count = 0
+
+		# # Calculate the moving average
+		avg_count += adc_value
+		moving_avg = avg_count / loop_counter
+		loop_counter += 1
+
+		#print(adc_value)
+		#print(moving_avg)
+
+		# Append the 10 bit voltage value
+		#samples.append(adc_value)
+
+		# check if the escape button is pressed
+		if GPIO.input(12) == GPIO.HIGH:
+			print("\n\nDrumTime is now exiting")
+			break
+
+		print(adc_value)
+
+		# End of Loop
+		time.sleep(interval)
+
+thread1 = threading.Thread(target=loopADC)
+thread2 = threading.Thread(target=loopScreen)
+# Start the threads
+thread1.start()
+thread2.start()
+
+# Record the end time of the loop
+endDate = datetime.now()
+
+# check if output exists and delete if it does
+if os.path.exists("output.txt"):
+	os.remove("output.txt")
+
+# Write output to file
+f = open("output.txt", "a")
+deltaDate = endDate - startDate
+# Write the time in seconds of recording
+f.write("d: " + str(deltaDate.total_seconds()) + "," + str(loop_count) + "\n")
+# Continue writing the data
+for sample in samples:
+	f.write(str(sample) + "\n")
+f.close()
+
+# Print the final data
+print("count " + str(loop_count) + " samples")
+print("time: " + str(deltaDate.total_seconds()) + "s")
---
+++ newscreen.py
@@ -0,0 +1,127 @@
+# Imports
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
+import Adafruit_SSD1306
+import board
+import math
+import digitalio
+import os
+import time
+import RPi.GPIO as GPIO
+from datetime import datetime
+
+# GPIO info
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(19, GPIO.OUT, initial=GPIO.LOW) # LED
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # BUTTON
+
+# Define the Reset Pin
+#oled_reset = digitalio.DigitalInOut(board.D18)
+oled_reset = 18
+
+# OLED info
+WIDTH = 128
+HEIGHT = 64  # Change to 64 if needed
+BORDER = 2
+
+# DESIGN INFO
+DIVIDER_HEIGHT = 2
+DIVIDER_WIDTH = 0
+
+STROKE_HEIGHT = 10
+STROKE_WIDTH = 2
+
+# 128x64 display with hardware I2C:
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=oled_reset, i2c_address=0x3D)
+
+# Begin the OLED session
+disp.begin()
+
+# Function to clear the display
+def clearDisplay():
+	disp.clear()
+	disp.display()
+
+# Clear initially
+clearDisplay()
+
+# Final Variables
+fps = 10
+fps_int = 1/fps # Interval for FPS in seconds
+
+# Create image buffer
+image = Image.new('1', (WIDTH, HEIGHT))
+
+# Variable for tracking time
+loop_count = 0
+time_window = 2 # Seconds to show in the view
+
+# Create drawing object.
+draw = ImageDraw.Draw(image)
+
+# The stroke objects stored as array pairs of [[Hand, Stroke Time]
+strokes = [["L", 0.5], ["R", 0.5], ["R", 0.75], ["L", 1], ["L", 1.5]]
+
+# Load default font.
+font = ImageFont.load_default()
+
+def loopScreen():
+	global draw
+	global loop_count
+	global fps
+	global time_window
+	global WIDTH
+	global HEIGHT
+	global DIVIDER_WIDTH
+	global strokes
+	global image
+	global disp
+	global font
+	global fps_int
+
+
+	# Begin While Loop
+	while(True):
+		# Clear the display
+		# Clear image buffer by drawing a black filled box.
+		draw.rectangle((0,0, WIDTH, HEIGHT), outline=0, fill=0)
+
+		# Draw the static divider
+		draw.rectangle((0, HEIGHT // 2, WIDTH, HEIGHT // 2), outline=255, fill=255)
+
+		# Get the current scaled time
+		scaled_time = loop_count / (fps * time_window)
+
+		# Draw the dynamic divider on the time axis
+		DIVIDER_WIDTH = math.floor(scaled_time * WIDTH)
+		draw.rectangle((0, HEIGHT // 2 - DIVIDER_HEIGHT, DIVIDER_WIDTH, HEIGHT // 2 + DIVIDER_HEIGHT), outline=255, fill=255)
+
+		# Draw the strokes dynamically
+		for stroke in strokes:
+			stroke_time = (stroke[1] / time_window) * WIDTH
+			if stroke[0] == "L": # Top of Screen
+				draw.rectangle((stroke_time, STROKE_HEIGHT, stroke_time, STROKE_HEIGHT * 2), outline=255, fill=255)
+			else:
+				draw.rectangle((stroke_time, (HEIGHT - STROKE_HEIGHT), stroke_time, (HEIGHT - STROKE_HEIGHT * 2)), outline=255, fill=255)
+
+		# Draw the L & R identifiers
+		draw.text((2, 2), "L", font=font, fill=255)
+		draw.text((2, HEIGHT - 9), "R", font=font, fill=255)
+
+		# Display the image
+		disp.image(image)
+		disp.display()
+
+		# check if the escape button is pressed
+		if GPIO.input(12) == GPIO.HIGH:
+			print("\n\nDrumTime is now exiting")
+			clearDisplay()
+			break
+
+		# Reset the dynamic counter
+		if loop_count >= time_window * fps:
+			loop_count = 0
+
+		loop_count += 1 # Increment the loop count
+		time.sleep(fps_int) # Rest for 1/fps
